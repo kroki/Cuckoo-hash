da20fec575470b1690cb882751066e76bec4989a
Tomash Brechko <tomash.brechko@gmail.com>
2011-12-21 15:36:54 +0400

    Improve version.sh.

--- version.sh
+++ version.sh
@@ -1,13 +1,13 @@
 #! /bin/sh
 
 if test -d .git; then
-    TAG=`git describe HEAD`
-    if git diff --quiet HEAD; then
-        DIRTY=
-    else
-        DIRTY=-dirty
+    TAG=`git describe --dirty 2>/dev/null`
+    if [ $? != 0 ]; then
+        COUNT=$[$(git rev-list --count HEAD) - 1]
+        TAG="0.0-$COUNT-g$(git describe --dirty --always)"
     fi
-    echo "m4_define([VERSION_STRING], [$TAG$DIRTY])" > version.m4.tmp
+    TAG=`echo $TAG | sed -e '/-.*-/! s/\(-\|$\)/-0\1/; s/-/_/g; s/_/-/'`
+    echo "m4_define([VERSION_STRING], [$TAG])" > version.m4.tmp
     if test -e version.m4 && cmp -s version.m4.tmp version.m4; then
         rm version.m4.tmp
     else
